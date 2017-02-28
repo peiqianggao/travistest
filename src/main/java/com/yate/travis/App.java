d5d80051a920f26d22c7fd11aa111c9847a8bbfa
gaopq <peiqianggao@@iaskdoc.com>
2017-02-28 10:30:48 +0800

    init

---
+++ src/main/java/com/yate/travis/App.java
@@ -0,0 +1,11 @@
+package com.yate.travis;
+
+/**
+ * Created by gaopq on 2/28/2017.
+ * Description:
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/yate/travis/AppTest.java
@@ -0,0 +1,11 @@
+package com.yate.travis;
+
+/**
+ * Created by gaopq on 2/28/2017.
+ * Description:
+ */
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
